3c29b03cb34ad016e71cfe3af2b0ab4b513302a1
Krovannox <krovannox@proton.me>
2025-10-04 14:35:18 -0500

    Add function to compare the two values
    The function uses nested switch statements to compare the input by the
    user and the computer, which then gives a console log to the results
    and add a value to the score of the computer or the user depending on
    the results of the comparison.

--- script.js
+++ script.js
@@ -29,5 +29,65 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+//Compare the two values
+function playRound(humanChoice, computerChoice) {
+    console.log(`Your choice: ${humanChoice}`);
+    console.log(`Computer choice: ${computerChoice}`);
+    
+    switch (humanChoice) {
+        case 'rock':
+            switch (computerChoice) {
+                case 'rock':
+                    console.log(`It's a tie! Rock ties with Rock`);
+                    break;
+                case 'paper':
+                    console.log(`You lose! Paper beats Rock`);
+                    computerScore++;
+                    break;
+                case 'scissors':
+                    console.log(`You win! Rock beats Scissors`);
+                    humanScore++;
+                    break;
+            }
+            break;
+        case 'paper':
+            switch (computerChoice) {
+                case 'rock':
+                    console.log(`You win! Paper beats Rock`);
+                    humanScore++;
+                    break;
+                case 'paper':
+                    console.log(`It's a tie! Paper ties with Paper`);
+                    break;
+                case 'scissors':
+                    console.log(`You lose! Scissors beats Paper`);
+                    computerScore++;
+                    break;
+            }
+            break;
+        case 'scissors':
+            switch (computerChoice) {
+                case 'rock':
+                    console.log(`You lose! Rock beats Scissors`);
+                    computerScore++;
+                    break;
+                case 'paper':
+                    console.log(`You win! Rock beats Scissors`);
+                    humanScore++;
+                    break;
+                case 'scissors':
+                    console.log(`It's a tie! Scissors ties with Scissors`);
+                    break;
+            }
+            break;
+    }
+
+    console.log(`Human Score: ${humanScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
 //TESTING
 playRound(humanSelection, computerSelection);
