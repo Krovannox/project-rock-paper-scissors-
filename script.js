0db1eae594e5b9223d124939210a3f3cc3f4fa8d
Krovannox <krovannox@proton.me>
2026-01-17 14:30:50 -0600

    Add display for game of 5 rounds results
    Added a few extra HTML displays for tracking the game.
    
    The buttons get disabled after 5 rounds to stop the game.

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
+let playerScore = 0;
+let computerScore = 0;
+let roundCounter = 0;
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
 
+const playerChoiceDisplay = document.querySelector('#player-choice-display');
+const computerChoiceDisplay = document.querySelector('#computer-choice-display');
+const roundNumberDisplay = document.querySelector('#round-number-display');
+const resultDisplay = document.querySelector('#result-display');
+
 function playRound(playerChoice) {
     const computerChoice = getComputerChoice();
 
-    const playerChoiceDisplay = document.querySelector('#player-choice-display');
     playerChoiceDisplay.textContent = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
-    console.log(`Player choice: ${playerChoice}`);
-
-    const computerChoiceDisplay = document.querySelector('#computer-choice-display');
     computerChoiceDisplay.textContent = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-    console.log(`Computer choice: ${computerChoice}`);
-
-    const resultDisplay = document.querySelector('#result-display');
+    roundNumberDisplay.textContent = roundCounter + 1;
 
     if (computerChoice === playerChoice) {
         resultDisplay.textContent = 'Tie';
-        console.log('Tie');
     } else if (
         (playerChoice === 'rock' && computerChoice === 'scissors') ||
         (playerChoice === 'paper' && computerChoice === 'rock') ||
         (playerChoice === 'scissors' && computerChoice === 'paper')
     ) {
         resultDisplay.textContent = 'Victory';
-        console.log('Victory');
+        playerScore += 1;
     } else {
         resultDisplay.textContent = 'Defeat';
-        console.log('Defeat');
+        computerScore += 1;
     }
 }
+
+const playerScoreDisplay = document.querySelector('#player-score-display');
+const computerScoreDisplay = document.querySelector('#computer-score-display');
+const gameResults = document.querySelector('.game-result');
 
 const gameButtons = document.querySelectorAll('.game-button');
 gameButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
         playRound(e.currentTarget.dataset.value);
+        roundCounter += 1;
+        playerScoreDisplay.textContent = playerScore;
+        computerScoreDisplay.textContent = computerScore;
+
+        if (roundCounter === 5) {
+            gameButtons.forEach(button => button.disabled = true);
+            const gameResultDisplay = document.createElement('p');
+            
+            if (playerScore > computerScore) {
+                gameResultDisplay.textContent = 'You won the game!';
+            } else if (computerScore > playerScore) {
+                gameResultDisplay.textContent = 'You lost the game!';
+            } else if (computerScore === playerScore) {
+                gameResultDisplay.textContent = 'The game was a tie!';
+            }
+
+            gameResults.appendChild(gameResultDisplay);
+        }
     });
 });
-/*
-
-//Create variables for score
-let humanScore = 0;
-let computerScore = 0;
-let tieScore = 0;
-
-//Make a function to play 5 rounds
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
-    if (humanScore > computerScore) {
-        console.log(`You WON with a score of ${humanScore} against the computer score of ${computerScore}`);
-    } else if (computerScore > humanScore) {
-        console.log(`You LOST with a score of ${humanScore} against the computer score of ${computerScore}`);
-    }
-} */
-
-//playGame();
