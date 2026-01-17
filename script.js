2bfa2814bb5e3f109bf1c697c8f951a06a2e2a9e
Krovannox <krovannox@proton.me>
2026-01-17 13:20:17 -0600

    Simplify logic for the game
    The switch-case logic was replace for a shorter if comparison.
    Ran tests with console.log to ensure it get the correct result.

--- script.js
+++ script.js
@@ -7,8 +7,20 @@ function getComputerChoice() {
 function playRound(playerChoice) {
     const computerChoice = getComputerChoice();
 
-    console.log(`Computer choice: ${computerChoice}`);
     console.log(`Player choice: ${playerChoice}`);
+    console.log(`Computer choice: ${computerChoice}`);
+
+    if (computerChoice === playerChoice) {
+        console.log('Tie');
+    } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        console.log('Victory');
+    } else {
+        console.log('Defeat');
+    }
 }
 
 const gameButtons = document.querySelectorAll('.game-button');
@@ -19,77 +31,10 @@ gameButtons.forEach((button) => {
 });
 /*
 
-//Ask user input
-function getHumanChoice() {
-    let humanChoice = (prompt('Input your choice: ')).toLowerCase();
-    return humanChoice;
-}
-
 //Create variables for score
 let humanScore = 0;
 let computerScore = 0;
 let tieScore = 0;
-
-//Compare the two values
-function playRound(humanChoice, computerChoice) {
-    console.log(`Your choice: ${humanChoice}`);
-    console.log(`Computer choice: ${computerChoice}`);
-    
-    switch (humanChoice) {
-        case 'rock':
-            switch (computerChoice) {
-                case 'rock':
-                    console.log(`It's a tie! Rock ties with Rock`);
-                    tieScore++;
-                    break;
-                case 'paper':
-                    console.log(`You lose! Paper beats Rock`);
-                    computerScore++;
-                    break;
-                case 'scissors':
-                    console.log(`You win! Rock beats Scissors`);
-                    humanScore++;
-                    break;
-            }
-            break;
-        case 'paper':
-            switch (computerChoice) {
-                case 'rock':
-                    console.log(`You win! Paper beats Rock`);
-                    humanScore++;
-                    break;
-                case 'paper':
-                    console.log(`It's a tie! Paper ties with Paper`);
-                    tieScore++;
-                    break;
-                case 'scissors':
-                    console.log(`You lose! Scissors beats Paper`);
-                    computerScore++;
-                    break;
-            }
-            break;
-        case 'scissors':
-            switch (computerChoice) {
-                case 'rock':
-                    console.log(`You lose! Rock beats Scissors`);
-                    computerScore++;
-                    break;
-                case 'paper':
-                    console.log(`You win! Rock beats Scissors`);
-                    humanScore++;
-                    break;
-                case 'scissors':
-                    console.log(`It's a tie! Scissors ties with Scissors`);
-                    tieScore++;
-                    break;
-            }
-            break;
-    }
-
-    console.log(`Human Score: ${humanScore}`);
-    console.log(`Computer Score: ${computerScore}`);
-    console.log(`Ties Score: ${tieScore}`);
-}
 
 //Make a function to play 5 rounds
 function playGame() {
